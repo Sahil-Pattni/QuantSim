9c56e698d3e1b66eb2ffa119bb6a9d44b2fc94b5
Sahil Pattni <sahilpattni97@gmail.com>
2023-05-18 17:40:17 +0400

    Fixed issue with abstract decorators in Strategy

--- src/strategies/strategy.py
+++ src/strategies/strategy.py
@@ -1,5 +1,17 @@
 from abc import ABC, abstractmethod
+import datetime
+from enum import Enum
+from utils.custom_exceptions import InvalidTradeException
 from utils.data import Data
+
+
+class Trades(Enum):
+    """
+    An enum for the types of trades.
+    """
+
+    BUY = 0
+    SELL = 1
 
 
 class Strategy(ABC):
@@ -7,18 +19,23 @@ class Strategy(ABC):
     An abstract class for backtesting strategies.
     """
 
-    @abstractmethod
-    def __init__(self, data) -> None:
+    def __init__(self, data_source: str, data_type: Data, capital: float) -> None:
         """
         Initialize the strategy with data.
 
         Args:
-            data (iterable): Data to be used in the strategy.
-        """
-        self.data = Data(data)
+            data_source (str): Data source to be used in the strategy.
+            data_type (Data): The type of data to use.
+            capital (float): The starting capital.
 
-    @abstractmethod
-    def execute(self):
+        """
+        self.data = data_type(data_source)
+        # Initialize the assets
+        self.assets = {
+            "BASE": capital,
+        }
+
+    def execute(self, start_date: datetime, end_date: datetime):
         """
         Execute the backtesting strategy.
 
@@ -28,7 +45,49 @@ class Strategy(ABC):
         Yields:
             tuple: A tuple containing the iteration index and the strategy result.
         """
-        pass
+        for i, datum in self.data.get_data(start_date, end_date).iterrows():
+            yield i, self.process_data(datum)
+
+    def buy(self, ticker: str, price: float, amount: float):
+        """
+        Buy a certain amount of a ticker.
+
+        Args:
+            ticker (str): The ticker to buy.
+            price (float): The price to buy at.
+            amount (float): The amount to buy.
+
+        Raises:
+            InvalidTradeException: If the trade is invalid.
+        """
+        base_equivalent = amount * price
+        # If we don't have enough money, raise an exception
+        if self.assets["BASE"] < base_equivalent:
+            raise InvalidTradeException(ticker, amount, True)
+
+        # Otherwise, execute the trade
+        self.assets["BASE"] -= base_equivalent
+        self.assets[ticker] += amount
+
+    def sell(self, ticker: str, price: float, amount: float):
+        """
+        Sell a certain amount of a ticker.
+
+        Args:
+            ticker (str): The ticker to sell.
+            price (float): The price to sell at.
+            amount (float): The amount to sell.
+
+        Raises:
+            InvalidTradeException: If the trade is invalid.
+        """
+        # If we don't have enough of the ticker, raise an exception
+        if self.assets[ticker] < amount:
+            raise InvalidTradeException(ticker, amount, False)
+
+        # Otherwise, execute the trade
+        self.assets["BASE"] += amount * price
+        self.assets[ticker] -= amount
 
     @abstractmethod
     def process_data(self, datum):
@@ -43,33 +102,5 @@ class Strategy(ABC):
 
         Returns:
             any: The result of processing the datum.
-        """
-        pass
-
-    @abstractmethod
-    def buy(self, ticker: str, amount: float):
-        """
-        Buy a certain amount of a ticker.
-
-        Args:
-            ticker (str): The ticker to buy.
-            amount (float): The amount to buy.
-
-        Raises:
-            InvalidTradeException: If the trade is invalid.
-        """
-        pass
-
-    @abstractmethod
-    def sell(self, ticker: str, amount: float):
-        """
-        Sell a certain amount of a ticker.
-
-        Args:
-            ticker (str): The ticker to sell.
-            amount (float): The amount to sell.
-
-        Raises:
-            InvalidTradeException: If the trade is invalid.
         """
         pass
